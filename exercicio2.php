066ea1cbeaa8af3b1dd6369d91b28959e64bd18b
artigasleo <53630087+artigasleo@users.noreply.github.com>
2019-08-16 22:00:31 -0300

    exercicio2

---
+++ exercicio2.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$celsius = readline ("digite temperatura em graus celsius: ");
+$fahrenheit = ((9* $celsius + 160)/5);
+$resultado = $fahrenheit;
+echo "A temperatura em fahrenheit é:  " . $resultado . PHP_EOL;
+
+?>
